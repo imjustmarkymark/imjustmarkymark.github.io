b24bcdd940b6618d0ec3999b9faeef295797f470
imjustmarkymark <72843696+imjustmarkymark@users.noreply.github.com>
2021-01-20 12:45:13 -0500

    Update index.js

--- bodymovin_test/index.js
+++ bodymovin_test/index.js
@@ -11,7 +11,7 @@ var animation = bodymovin.loadAnimation({
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'selfie.json'
+    path: 'bodymovin_test/selfie.json'
 })
 
 document.getElementById('js_test').innerHTML = "testing JS success. 2!";
