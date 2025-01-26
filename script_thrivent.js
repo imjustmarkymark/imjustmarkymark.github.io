ad19fe50fb9c7314c0fd8e91368e7aa16fb0bfd0
imjustmarkymark <72843696+imjustmarkymark@users.noreply.github.com>
2025-01-25 23:53:19 -0800

    Mobile friendly thrivent example sets
    Made the dynamic before and after section of thrivent only work on desktop, and be static on mobile.

--- script_thrivent.js
+++ script_thrivent.js
@@ -6,40 +6,41 @@ document.getElementById('after4').addEventListener('mouseover', growAfter.bind(n
 // document.getElementById('after5').addEventListener('mouseover', growAfter.bind(null, "5"));
 
 function growAfter(num) {
-    // Grow after section
-    // console.log("worked");
-    document.getElementById('after' + num).style.transition = "all .5s ease";
-    document.getElementById('after' + num).style.top = "48px";
-    document.getElementById('after' + num).style.rotate = "0deg";
-    document.getElementById('after' + num).style.right = "0%";
-    document.getElementById('after' + num).style.scale = "100%";
-    document.getElementById('after' + num).style.zIndex = "2";
 
-    setTimeout(`document.getElementById('example_set${num}').style.paddingBottom = '120px'`, 250);
+    if (window.innerWidth > 537) {
 
-    // document.getElementById('example_set1').style.paddingBottom = "120px";
-    
+        // Grow after section
+        document.getElementById('after' + num).style.transition = "all .5s ease";
+        document.getElementById('after' + num).style.top = "48px";
+        document.getElementById('after' + num).style.rotate = "0deg";
+        document.getElementById('after' + num).style.right = "0%";
+        document.getElementById('after' + num).style.scale = "100%";
+        document.getElementById('after' + num).style.zIndex = "2";
 
-    // Shrink before section
-    document.getElementById("before" + num).style.transition = "all .5s ease";
-    document.getElementById('before' + num).style.rotate = "-17deg";
-    document.getElementById('before' + num).style.scale = "40%";
-    document.getElementById('before' + num).style.right = "20%";
-    document.getElementById('before' + num).style.top = "100px";
+        setTimeout(`document.getElementById('example_set${num}').style.paddingBottom = '120px'`, 250);
+
+        // document.getElementById('example_set1').style.paddingBottom = "120px";
+        
+
+        // Shrink before section
+        document.getElementById("before" + num).style.transition = "all .5s ease";
+        document.getElementById('before' + num).style.rotate = "-17deg";
+        document.getElementById('before' + num).style.scale = "40%";
+        document.getElementById('before' + num).style.right = "20%";
+        // document.getElementById('before' + num).style.top = "0px";
 
 
+        document.getElementById("text_before" + num).style.transition = "opacity .25s ease";
+        document.getElementById("text_before" + num).style.opacity = "0";
+        setTimeout("document.getElementById('text_before" + num + "').style.display = 'none';", 250);
+        // document.getElementById('text_before1').style.display = "none";
 
-
-    document.getElementById("text_before" + num).style.transition = "opacity .25s ease";
-    document.getElementById("text_before" + num).style.opacity = "0";
-    setTimeout("document.getElementById('text_before" + num + "').style.display = 'none';", 250);
-    // document.getElementById('text_before1').style.display = "none";
-
-    setTimeout("document.getElementById('text_after" + num + "').style.display = 'block';", 250);
-    // document.getElementById('text_after1').style.display = "block";
-    document.getElementById("text_after" + num).style.transition = "opacity .25s ease";
-    setTimeout("document.getElementById('text_after" + num + "').style.opacity = '1';", 300);
-    // document.getElementById("text_after1").style.opacity = "1";
+        setTimeout("document.getElementById('text_after" + num + "').style.display = 'block';", 250);
+        // document.getElementById('text_after1').style.display = "block";
+        document.getElementById("text_after" + num).style.transition = "opacity .25s ease";
+        setTimeout("document.getElementById('text_after" + num + "').style.opacity = '1';", 300);
+        // document.getElementById("text_after1").style.opacity = "1";
+    }
 }
 
 document.getElementById('before1').addEventListener('mouseover', growBefore.bind(null, "1"));
@@ -50,31 +51,40 @@ document.getElementById('before4').addEventListener('mouseover', growBefore.bind
 
 
 function growBefore(num) {
-    // Grows before section 
-    document.getElementById('before' + num).style.rotate = "0deg";
-    document.getElementById('before' + num).style.scale = "100%";
-    document.getElementById('before' + num).style.right = "0%";
-    document.getElementById('before' + num).style.top = "0px";
+    if (window.innerWidth > 537) {
 
-    // Fix spacing at bottom
-    setTimeout("document.getElementById('example_set" + num + "').style.paddingBottom = '0px'", 250);
+        // Grows before section 
+        document.getElementById('before' + num).style.rotate = "0deg";
+        document.getElementById('before' + num).style.scale = "100%";
+        document.getElementById('before' + num).style.right = "0%";
+        // document.getElementById('before' + num).style.top = "0px";
 
-    // Shrinks after section
-    document.getElementById('after' + num).style.bottom = "0%";
-    document.getElementById('after' + num).style.top = "0";
-    document.getElementById('after' + num).style.rotate = "17deg";
-    document.getElementById('after' + num).style.right = "-20%";
-    document.getElementById('after' + num).style.scale = "40%";
-    document.getElementById('after' + num).style.zIndex = "0";
+        // Fix spacing at bottom
+        setTimeout("document.getElementById('example_set" + num + "').style.paddingBottom = '0px'", 250);
 
-    document.getElementById("text_after" + num).style.transition = "opacity .25s ease";
-    document.getElementById("text_after" + num).style.opacity = "0";
-    setTimeout("document.getElementById('text_after" + num + "').style.display = 'none';", 250);
+        // Shrinks after section
+        document.getElementById('after' + num).style.bottom = "0%";
+        document.getElementById('after' + num).style.top = "0";
+        document.getElementById('after' + num).style.rotate = "17deg";
+        document.getElementById('after' + num).style.right = "-20%";
+        document.getElementById('after' + num).style.scale = "40%";
+        document.getElementById('after' + num).style.zIndex = "0";
 
-    setTimeout("document.getElementById('text_before" + num + "').style.display = 'block';", 250);
-    document.getElementById("text_before" + num).style.transition = "opacity .25s ease";
-    setTimeout("document.getElementById('text_before" + num + "').style.opacity = '1';", 300);
+        document.getElementById("text_after" + num).style.transition = "opacity .25s ease";
+        document.getElementById("text_after" + num).style.opacity = "0";
+        setTimeout("document.getElementById('text_after" + num + "').style.display = 'none';", 250);
+
+        setTimeout("document.getElementById('text_before" + num + "').style.display = 'block';", 250);
+        document.getElementById("text_before" + num).style.transition = "opacity .25s ease";
+        setTimeout("document.getElementById('text_before" + num + "').style.opacity = '1';", 300);
+        }
 }
+
+// Move example text for static view in mobile
+// var text = document.getElementById('text_before1');
+// var gif = document.getElementById('after1');
+// var parent = text.parentNode;
+// parent.insertBefore(text, gif);
 
 
 document.addEventListener('scroll', addShadow);
@@ -89,53 +99,3 @@ function addShadow() {
         document.getElementById("nav_header").style.boxShadow = "";
     }
 }
-
-// Nav buttons
-document.getElementById('navOneHeader').addEventListener('click', showHeader1);
-document.addEventListener('click', hideHeader1);
-document.getElementById('navTwoHeader').addEventListener('click', showHeader2);
-document.addEventListener('click', hideHeader2);
-
-function showHeader1() {
-    document.getElementById('navOneOptions').style.opacity = "1";
-    document.getElementById('navOneOptions').style.zIndex = "1";
-    document.getElementById('navOneOptions').style.transform = "translateY(0px)";
-}
-
-function hideHeader1(evt) {
-    var clickIn = document.getElementById('navOneHeader');
-    targetElement = evt.target;
-
-    do {
-        if (targetElement == clickIn) {
-            return;
-        }
-        targetElement = targetElement.parentNode;
-    } while (targetElement);
-    
-    document.getElementById('navOneOptions').style.opacity = "0";
-    document.getElementById('navOneOptions').style.zIndex = "-1";
-    document.getElementById('navOneOptions').style.transform = "translateY(-10px)";
-    }
-
-function showHeader2() {
-    document.getElementById('navTwoOptions').style.opacity = "1";
-    document.getElementById('navTwoOptions').style.zIndex = "1";
-    document.getElementById('navTwoOptions').style.transform = "translateY(0px)";
-}
-
-function hideHeader2(evt) {
-    var clickIn = document.getElementById('navTwoHeader');
-    targetElement = evt.target;
-
-    do {
-        if (targetElement == clickIn) {
-            return;
-        }
-        targetElement = targetElement.parentNode;
-    } while (targetElement);
-    
-    document.getElementById('navTwoOptions').style.opacity = "0";
-    document.getElementById('navTwoOptions').style.zIndex = "-1";
-    document.getElementById('navTwoOptions').style.transform = "translateY(-10px)";
-    }
