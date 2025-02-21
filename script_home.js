22a51e72628233ff1e4f15e456d9b9811f2fe035
imjustmarkymark <72843696+imjustmarkymark@users.noreply.github.com>
2025-02-20 17:45:26 -0800

    Added testimony section

--- script_home.js
+++ script_home.js
@@ -25,3 +25,64 @@ function equateDropdown() {
 }
 
 document.getElementById('dropdownLabel').addEventListener('mouseover', equateDropdown);
+
+
+
+// Carousel for testimonies
+const container = document.querySelector('.testimony_container');
+const items = document.querySelector('.testimony_items');
+const prevButton = document.querySelector('.testimony_prev');
+const nextButton = document.querySelector('.testimony_next');
+
+let counter = 0;
+let itemWidth = container.offsetWidth;
+
+// Function to update itemWidth dynamically based on container width
+function updateItemWidth() {
+    itemWidth = container.offsetWidth;
+    items.style.transform = `translateX(-${itemWidth * counter}px)`;  
+  }
+  
+// Update the itemWidth whenever the window is resized
+window.addEventListener('resize', updateItemWidth);
+
+function slideNext() {
+    counter++;
+    if (counter >= items.children.length) {
+        counter = 0;
+    }
+    items.style.transform = `translateX(-${itemWidth * counter}px)`;  
+}
+
+function slidePrevious() {
+    counter--;
+    if (counter < 0) {
+      counter = items.children.length - 1;
+    }
+    items.style.transform = `translateX(-${itemWidth * counter}px)`;
+  }
+
+// Event listeners for buttons
+  nextButton.addEventListener('click', slideNext);
+  prevButton.addEventListener('click', slidePrevious);
+  
+let touchStartX = 0;
+let touchEndX = 0;
+
+container.addEventListener('touchstart', (e) => {
+  touchStartX = e.changedTouches[0].screenX;
+});
+
+container.addEventListener('touchend', (e) => {
+  touchEndX = e.changedTouches[0].screenX;
+  handleSwipe();
+});
+
+function handleSwipe() {
+  if (touchEndX < touchStartX) {
+    slideNext(); // Swipe left, go to the next slide
+  }
+  if (touchEndX > touchStartX) {
+    slidePrevious(); // Swipe right, go to the previous slide
+  }
+}
